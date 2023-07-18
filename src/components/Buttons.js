509d7bd683bba898c6765a5d7eec08e257b8610d
monicasrinivasan27 <srinivasan.monica@gmail.com>
2023-07-18 08:37:12 -0500

    Updated for the orbit report.

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,24 @@
-const Buttons = () => {
-  return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
-  );
-};
+import satData from "./satData";
+import './styling.css';
 
+const Buttons = (props) => {
+  //filterByType, setSat, and displaySats
+  //console.log(props.displaySats);
+  //console.log("helo");
+  return(
+  <div className="flex-container">
+    {props.displaySats.map((sat, id) => {
+  return (
+    <button onClick={() => props.filterByType(sat)} key={id}>
+      {sat} Orbit
+    </button>
+  );
+    })}
+        <button onClick={() =>props.setSat(satData)}>
+          All Orbit
+        </button>
+    </div>
+  );
+}
 export default Buttons;
+/*Inside the <button> tag, create an onClick function that points to setSat. Pass satData to setSat.*/
